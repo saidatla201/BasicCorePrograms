44b345e496b8a4e2dd53f5208a970e9ca3e99013
saidatla201 <saidatla201@gmail.com>
2022-12-27 21:37:22 +0530

    Adding Largest among three numbers program

--- Program.cs
+++ Program.cs
@@ -10,15 +10,28 @@ namespace logicprograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the dividend value :");
-            int numerator = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the divisor vale:");
-            int denominator = Convert.ToInt32(Console.ReadLine());
-
-            int quotient = numerator / denominator;
-            int remainder = numerator % denominator;
-
-            Console.WriteLine("The quotient is {0} and the remainder is {1}", quotient, remainder);
+            Console.WriteLine("Enter 1st number :");
+            int first= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter  2nd number :");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 3rd number");
+            int third = Convert.ToInt32(Console.ReadLine());
+            if(first>second)
+            {
+                if(first >third )
+                {
+                    Console.WriteLine("The first number is largest among three");
+                }
+                
+            }
+            else if (second>third)
+            {
+                Console.WriteLine("The second number is largest among three");
+            }
+            else 
+            {
+                Console.WriteLine("The third number is largest among three");
+            }
             Console.ReadLine();
         }
     }
